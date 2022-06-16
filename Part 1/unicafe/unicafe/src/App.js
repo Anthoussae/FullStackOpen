728fc48b5cc4440fdbce09ebabf2951a393a901c
Anthoussae <45543733+Anthoussae@users.noreply.github.com>
2022-06-15 17:55:55 -0700

    1.9 homework

--- Part 1/unicafe/unicafe/src/App.js
+++ Part 1/unicafe/unicafe/src/App.js
@@ -1,13 +1,13 @@
+//ex 1.9
+
 import { useState } from 'react'
 import './index.css';
 
-
 const Button = (props) => (
-  <button onClick={props.handleClick}>
+  <button onClick={props.handleClick} id={props.id}>
   {props.buttonName}  
   </button>
 )
-const Display = (props) => <div className='Display'>{props.name} {props.value}</div>
 
 const Header = (props) => (
  <div className='Header'>
@@ -17,40 +17,127 @@ const Header = (props) => (
  </div>
 )
 
+const Statistics = (props) => (
+  
+  <div>
+<div className='Header'>
+  <p> 
+     {props.topLabel}
+  </p>
+ </div>
+   <div className='Display'>{props.firstSubLabel} {props.firstValue}</div>
+   <div className='Display'>{props.secondSubLabel} {props.secondValue}</div>
+   <div className='Display'>{props.thirdSubLabel} {props.thirdValue}</div>
+   <div className='Display'>{props.fourthSubLabel} {props.fourthValue}</div>
+   <div className='Header'>
+  <p> 
+     {props.bottomLabel}
+  </p>
+ </div>
+   <div className='Display'>{props.fifthSubLabel} {props.fifthValue}</div>
+   <div className='Display'>{props.sixthSubLabel} {props.sixthValue}</div>
+ </div>
+  
+)
 
 const App = () => {
-  // save clicks of each button to its own state
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+  const [interacted, setInteracted] = useState(false)
 
   const goodReport = () => {
-    console.log('good');
+    console.log('good', good);
     setGood(good+1);
+    setAll(all+1);
+    setAverage(
+      (((good+1)+(bad*-1))/(all+1)).toFixed(2)
+      )
+    setPositive(
+      (((good+1)/(all+1))*100).toFixed(2)+"%"
+    )
+    setInteracted(true)
    }
    
   const neutralReport = () => {
-     console.log('neutral');
+     console.log('neutral', neutral);
      setNeutral(neutral+1);
+     setAll(all+1);
+     setAverage(
+      (((good)+(bad*-1))/(all+1)).toFixed(2)
+      )
+    setPositive(
+      (((good)/(all+1))*100).toFixed(2)+"%"
+    ) 
+    setInteracted(true) 
    }  
    
   const badReport = () => {
-     console.log('bad');
+     console.log('bad', bad);
      setBad(bad+1);
+     setAll(all+1);
+     setAverage(
+      (((good)+((bad+1)*-1))/(all+1)).toFixed(2)
+      )
+     setPositive(
+      (((good)/(all+1))*100).toFixed(2)+"%"
+    ) 
+    setInteracted(true)
    }
 
-  return (
+
+if (interacted){
+    return (
     <div>
-      <Header name='Give Feedback'></Header>
-      <Button buttonName='good' handleClick={goodReport}></Button>
-      <Button buttonName='neutral' handleClick={neutralReport}></Button>
-      <Button buttonName='bad' handleClick={badReport}></Button>
-      <Header name='Statistics'></Header>
-      <Display name='good' value={good}></Display>
-      <Display name='neutral' value={neutral}></Display>
-      <Display name='bad' value={bad}></Display>
+      
+      <Header name='Submit Feedback'></Header>
+      <Button buttonName='good' handleClick={goodReport} id='goodButton'></Button>
+      <Button buttonName='neutral' handleClick={neutralReport} id='neutralButton'></Button>
+      <Button buttonName='bad' handleClick={badReport} id='badButton'></Button>
+    
+      <Statistics topLabel='Basic Statistics' 
+
+        firstSubLabel='good'
+        secondSubLabel='neutral'
+        thirdSubLabel='bad'
+        fourthSubLabel='all'
+        firstValue={good}
+        secondValue={neutral}
+        thirdValue={bad}
+        fourthValue={all}
+
+        bottomLabel='Advanced Statistics'
+
+        fifthSubLabel='average' 
+        sixthSubLabel='positive' 
+        fifthValue={average} 
+        sixthValue={positive}
+
+        >
+
+      </Statistics>
+
     </div>
   )
+    }
+    else {
+      return (
+        <div>
+      
+      <Header name='Submit Feedback'></Header>
+      <Button buttonName='good' handleClick={goodReport} id='goodButton'></Button>
+      <Button buttonName='neutral' handleClick={neutralReport} id='neutralButton'></Button>
+      <Button buttonName='bad' handleClick={badReport} id='badButton'></Button>
+      <Header name='No Feedback Submitted Yet'></Header>
+      
+
+    </div>
+      )
+    }
 }
 
 export default App
