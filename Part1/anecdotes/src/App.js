2a294f56b448f141c9dda352d721234968827c5b
Anthoussae <45543733+Anthoussae@users.noreply.github.com>
2022-06-21 16:08:54 -0700

    updated with homework 1.13

--- Part1/anecdotes/src/App.js
+++ Part1/anecdotes/src/App.js
@@ -1,3 +1,6 @@
+//1.13
+
+import { render } from '@testing-library/react'
 import React from 'react'
 import { useState } from 'react'
 
@@ -13,8 +16,9 @@ const Display = (props) => (
   </div>
 )
 
-
 const App = () => {
+
+  //core 
   const anecdotes = [
     'If it hurts, do it more often',
     'Adding manpower to a late software project makes it later!',
@@ -25,39 +29,66 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
   ]
 
+  const votesArray = Array(anecdotes.length).fill(0);
+
+  //misc useful subfunctions
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
   }
 
+  function sum(arr) {
+    let total = 0;
+    arr.forEach(e => total = total+e);
+    return total;
+  }
+
+  //buttons
   function anecdoteButtonClick(){
     setInteracted(true);
-    console.log('pressed');
+    console.log('random anecdote generated');
     setSelected(getRandomInt(0,(anecdotes.length-1)));
   }
-   
-  const [selected, setSelected] = useState(0)
+
+  function voteButtonClick(){
+    setInteracted(true);
+    const votesCopy = [...votes];
+    votesCopy[selected] +=   1;
+    setVotes(votesCopy);
+  }
+  
+  //states
+  const [votes, setVotes] = useState(votesArray)
+  const [selected, setSelected] = useState(getRandomInt(0,(anecdotes.length-1)))
   const [interacted, setInteracted] = useState(false)
+
 
   if (interacted){
     return (
       <div>
-        <Button name='Random anecdote' onclick={anecdoteButtonClick}></Button>
-        <p></p>
         <Display displayContent={anecdotes[selected]}></Display>
+        <Display displayContent={"has " + votes[selected] + " votes"}></Display>
+        <Display displayContent={"total votes: " + sum(votes)}></Display>
+        <p></p>
+        <Button name='Random anecdote' onclick={anecdoteButtonClick}></Button>
+        <Button name='Vote' onclick={voteButtonClick}></Button>
       </div>
     )
   }
   else {
     return (
       <div>
-        <Button name='Random anecdote' onclick={anecdoteButtonClick}></Button>
+        <Display displayContent={anecdotes[selected]}></Display>
+        <Display displayContent={"has " + votes[selected] + " votes"}></Display>
+        <Display displayContent={"Please submit a vote or reroll the anecdote."}></Display>
         <p></p>
-        <Display displayContent='Press the button for an aphorism'></Display>
+        <Button name='Random anecdote' onclick={anecdoteButtonClick}></Button>
+        <Button name='Vote' onclick={voteButtonClick}></Button>
       </div>
     )
   }
 }
+
 
 export default App
